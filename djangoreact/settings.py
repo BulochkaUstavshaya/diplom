ef51c3dcd24703af636bd9877522922d687d966b
BulochkaUstavshaya <bezumiezzarazno@gmail.com>
2021-05-16 02:06:19 +0500

    'Update'

--- djangoreact/settings.py
+++ djangoreact/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-!h=i5)u#*b%4@9v)_g0(p5b!)*n)!ii=(s25r0$8s$*1lgkyyy'
+SECRET_KEY = 'django-insecure-!h=i5)u#*b%4@9v)_g0(p5b!)*n)!ii=(s25r0$8s$*1lgkyyy'
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-!h=i5)u#*b%4@9v)_g0(p5b!)*n)!ii=(s25r0$8s$*1lgkyyy')
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-!h=i5)u#*b%4@9v)_g0(p5b!)*n)!ii=(s25r0$8s$*1lgkyyy')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = False
+# DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
