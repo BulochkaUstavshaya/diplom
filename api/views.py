f3366b4dad080ea81b0792b6d83bba0d99a4d52b
BulochkaUstavshaya <bezumiezzarazno@gmail.com>
2021-05-16 22:11:11 +0500

    fixSave5

--- api/views.py
+++ api/views.py
@@ -110,6 +110,51 @@ class GetUserClothes(APIView):
         return Response(serializer.data)
 
 
+# БЫЛО ИЗНАЧАЛАЛЬНО
+# class SaveUserClothes(APIView):
+#     def post(self, request):
+#         try:
+#             access_token = request.data["access"]
+#             token = AccessToken(access_token)
+#             # check access token
+#             token.check_exp()
+#
+#             # if user have this clothes then BAN
+#             if UserClothes.objects.filter(linkSource=request.data["linkSource"]).first() is None: # вынеси в отдельную переменную
+#                 # insert clothes in db
+#                 # userclothes = UserClothes()
+#                 # userclothes.nameClothes = request.data["nameClothes"]
+#                 # userclothes.typeClothes = request.data["typeClothes"]
+#                 # userclothes.description = request.data["description"]
+#                 # userclothes.price = request.data["price"]
+#                 # userclothes.linkImage = request.data["linkImage"]
+#                 # userclothes.linkSource = request.data["linkSource"]
+#                 # userclothes.save()
+#                 userclothes = UserClothes.objects.create(nameClothes=request.data["nameClothes"],
+#                                                          typeClothes=request.data["typeClothes"],
+#                                                          description=request.data["description"],
+#                                                          price=request.data["price"],
+#                                                          linkImage=request.data["linkImage"],
+#                                                          linkSource=request.data["linkSource"]
+#                                                          )
+#             else:
+#                 userclothes = UserClothes.objects.filter(linkSource=request.data["linkSource"]).first()
+#
+#             user_id = TokenUser(token).id
+#             user = User.objects.filter(id=user_id).first()
+#             user.userclothes_set.add(userclothes)
+#
+#             return Response(status=status.HTTP_201_CREATED)
+#
+#         except Exception as e:
+#             # return Response({
+#             #     "detail": "Token is invalid or expired",
+#             #     "code": "token_not_valid"
+#             # })
+#             return Response({
+#                 "exeption": str(e)
+#             })
+
 class SaveUserClothes(APIView):
     def post(self, request):
         try:
@@ -117,18 +162,14 @@ class SaveUserClothes(APIView):
             token = AccessToken(access_token)
             # check access token
             token.check_exp()
+        except Exception as e:
+            return Response({
+                "exeption_token": str(e)
+            })
 
+        try:
             # if user have this clothes then BAN
             if UserClothes.objects.filter(linkSource=request.data["linkSource"]).first() is None: # вынеси в отдельную переменную
-                # insert clothes in db
-                # userclothes = UserClothes()
-                # userclothes.nameClothes = request.data["nameClothes"]
-                # userclothes.typeClothes = request.data["typeClothes"]
-                # userclothes.description = request.data["description"]
-                # userclothes.price = request.data["price"]
-                # userclothes.linkImage = request.data["linkImage"]
-                # userclothes.linkSource = request.data["linkSource"]
-                # userclothes.save()
                 userclothes = UserClothes.objects.create(nameClothes=request.data["nameClothes"],
                                                          typeClothes=request.data["typeClothes"],
                                                          description=request.data["description"],
@@ -139,15 +180,24 @@ class SaveUserClothes(APIView):
             else:
                 userclothes = UserClothes.objects.filter(linkSource=request.data["linkSource"]).first()
 
+        except Exception as e:
+            return Response({
+                "exeption_create_clothes": str(e)
+            })
+
+        try:
             user_id = TokenUser(token).id
             user = User.objects.filter(id=user_id).first()
-            user.userclothes_set.add(userclothes)
+        except Exception as e:
+            return Response({
+                "exeption_filter_user": str(e)
+            })
 
+        try:
+            user.userclothes_set.add(userclothes)
             return Response(status=status.HTTP_201_CREATED)
 
         except Exception as e:
             return Response({
-                "detail": "Token is invalid or expired",
-                "code": "token_not_valid"
+                "exeption_add_clothes_for_user": str(e)
             })
-
